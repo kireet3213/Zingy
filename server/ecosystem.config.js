22d4831ec5af551a94961bf7642fb31818dcc973
Kirit Joshi <92445686+kireet3213@users.noreply.github.com>
2024-11-03 18:31:52 +0530

    update ecosystem config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,3 +1,10 @@
+// const path = require('path');
+import { path } from 'node:path';
+import { config } from 'dotenv';
+config({
+    path: path.join(__dirname, '.env'),
+});
+
 module.exports = {
     apps: [
         {
@@ -6,6 +13,16 @@ module.exports = {
             watch: '.',
             env_production: {
                 NODE_ENV: 'production',
+                PORT: process.env.PORT,
+                DB_DIALECT: process.env.DB_DIALECT,
+                DB_HOST: process.env.DB_HOST,
+                DB_USERNAME: process.env.DB_USERNAME,
+                DB_PASSWORD: process.env.DB_PASSWORD,
+                DB_NAME: process.env.DB_NAME,
+                DB_LOGGER: process.env.DB_LOGGER,
+                JWT_SECRET: process.env.JWT_SECRET,
+                DB_PORT: process.env.DB_PORT,
+                CLIENT_URL: process.env.CLIENT_URL,
             },
             exec_mode: 'cluster',
             error_file: './',
